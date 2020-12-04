329998d01717e035acd222b109d689104e14c7eb
Robert Andreev <andreev.robert.a@gmail.com>
2020-12-05 01:12:30 +0300

    Add Outline var

--- Main.m
+++ Main.m
@@ -3,11 +3,76 @@ size50 = 50;
 size1000 = 1000; 
 odin = 1/(2^(0.5));
 x = -4:0.1:4;
+
+NormX1 = -2.698;
+NormX2 = 2.698;
+CaushyX1 = -4;
+CaushyX2 = 4;
+LaplasQ3 = sqrt(2)*log(2);
+LaplasQ1 = -sqrt(2)*log(2);
+LaplasX1 = LaplasQ1-1.5*(LaplasQ3-LaplasQ1);
+LaplasX2 = LaplasQ3+1.5*(LaplasQ3-LaplasQ1);
+PuassX1 = 2;
+PuassX2 = 18;
+UnifX1 = -3.464;
+UnifX2 = 3.464;
+
+percentOutline20 = [];
+percentOutline100 = [];
+for i = 1:1000
+    percentOutline20 = [percentOutline20 percentOfOutLine(normrnd(0,1,[1,20]),NormX1,NormX2)];
+    percentOutline100 = [percentOutline100 percentOfOutLine(normrnd(0,1,[1,100]),NormX1,NormX2)];   
+end
+disp("Нормальное")
+disp(average(percentOutline20));
+disp(average(percentOutline100));
+
+percentOutline20 = [];
+percentOutline100 = [];
+for i = 1:1000
+    percentOutline20 = [percentOutline20 percentOfOutLine(trnd(1,1,20),CaushyX1,CaushyX2)];
+    percentOutline100 = [percentOutline100 percentOfOutLine(trnd(1,1,100),CaushyX1,CaushyX2)];   
+end
+disp("Коши")
+disp(average(percentOutline20));
+disp(average(percentOutline100));
+
+
+percentOutline20 = [];
+percentOutline100 = [];
+for i = 1:1000
+    percentOutline20 = [percentOutline20 percentOfOutLine(laprnd(20),LaplasX1,LaplasX2)];
+    percentOutline100 = [percentOutline100 percentOfOutLine(laprnd(100),LaplasX1,LaplasX2)];   
+end
+disp("Лапласа")
+disp(average(percentOutline20));
+disp(average(percentOutline100));
+
+percentOutline20 = [];
+percentOutline100 = [];
+for i = 1:1000
+    percentOutline20 = [percentOutline20 percentOfOutLine(poissrnd(10,1,20),PuassX1,PuassX2)];
+    percentOutline100 = [percentOutline100 percentOfOutLine(poissrnd(10,1,100),PuassX1,PuassX2)];   
+end
+disp("Пуассон")
+disp(average(percentOutline20));
+disp(average(percentOutline100));
+
+percentOutline20 = [];
+percentOutline100 = [];
+for i = 1:1000
+    percentOutline20 = [percentOutline20 percentOfOutLine(unifrnd(-sqrt(3),sqrt(3),1,20),UnifX1,UnifX2)];
+    percentOutline100 = [percentOutline100 percentOfOutLine(unifrnd(-sqrt(3),sqrt(3),1,100),UnifX1,UnifX2)];   
+end
+disp("Равномерное")
+disp(average(percentOutline20));
+disp(average(percentOutline100));
+
+
 g1 = repmat({'100'},100,1);
 g2 = repmat({'20'},20,1);
 grp = [g1; g2];
 rng default  % For reproducibility
-
 n20 = normrnd(0,1,[1,20]);
 n100 = normrnd(0,1,[1,100]);
 boxNorm = [n100 n20];
@@ -42,7 +107,6 @@ boxUnif = [u100 u20];
 figure;
 boxplot(boxUnif,grp);
 title("Равномерное")
-
 
 %{
 array = [10 100 1000];
@@ -225,6 +289,7 @@ hold on;
 x33 = min(c3):0.1:max(c3);
 plot(x33,tpdf(x33,1));
 set(gca,'YScale','log')
+
 x3 = -6:0.1:6;
 l1 = laprnd(size10);
 l2 = laprnd(size50);
@@ -267,7 +332,7 @@ figure;
 histogram(p3,edges,'Normalization','pdf'); grid on; ylabel('density');
 hold on;
 plot(x4,poisspdf(x4,10));
-
+%}
 x5 = -3:0.05:3;
 
 r1 = unifrnd(-sqrt(3),sqrt(3),1,size10);
@@ -288,7 +353,7 @@ figure;
 histogram(r3,30,'Normalization','pdf'); grid on; ylabel('density');
 hold on;
 plot(x5,unifpdf(x5,-sqrt(3),sqrt(3)));
-%}
+
 
 function znach = lappdf(x, a, b)
     znach = (a/2)*exp(-a*abs(x-b));
@@ -296,18 +361,12 @@ end
 
 function res = laprnd(size)
 odin2 = 1/(2^(0.5));
-i = 0;
+i = 1;
 result = [];
-while i < size
-    %P = rand * odin2 * 0.5;
-    member = (rand * 50)-25; %-log(P*2*sqrt(2))*sqrt(2);
-    if (lappdf(member,odin2,0) / odin2 * 0.5) >= rand
-        i=i+1;
-        if rand > 0.5
-            member = -member;
-        end
-        result = [result member];
-    end
+arrOfRand = rand(1,size+1);
+while i < (size + 1)
+    result = [result (1/odin2)*log((arrOfRand(i)/(arrOfRand(i+1))))];
+    i = i + 1;
 end
 res = result;
 end
@@ -373,4 +432,20 @@ for i = 1:sz
     summa = summa + (arr(i)-srednee)^2;
 end
 result = summa / sz;
+end
+
+function result = percentOfOutLine(arr,X1,X2)
+sz = size(arr);
+sz = sz(2);
+
+numberOfOutLine = 0;
+for i = 1:sz
+    if (arr(i) < X1)
+        numberOfOutLine = numberOfOutLine + 1;
+    end
+    if (arr(i) > X2)
+        numberOfOutLine = numberOfOutLine + 1;
+    end
+end
+result = numberOfOutLine / sz;
 end
